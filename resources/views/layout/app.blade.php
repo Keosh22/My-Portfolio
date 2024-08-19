1c722558a4fcff9ae30dc6270f76f581a140b43e
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-08-19 19:39:08 +0700

    create layout folder

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite('resources/css/app.css')
+  <title>My Portfolio</title>
+</head>
+<body>
+  <h3 class="text-4xl font-bold">Hello World</h3>
+</body>
+</html>
