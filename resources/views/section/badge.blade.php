9d7d3f3a1f706f39ba62a1f228733f8ab2326e0d
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-03 00:46:16 +0700

    update: badge size in badge page

--- resources/views/section/badge.blade.php
+++ resources/views/section/badge.blade.php
@@ -4,13 +4,13 @@
     </div>
     <div class="flex lg:flex-row flex-col justify-center items-center gap-16 mt-14">
         <div class="bg-cambridge-400 shadow-lg">
-            <div class="flex-1" data-iframe-width="380" data-iframe-height="240"
+            <div class="flex-1" data-iframe-width="300" data-iframe-height="250"
                 data-share-badge-id="3a1be1cd-a15e-438b-b4d3-5bfe22c29eeb" data-share-badge-host="https://www.credly.com">
             </div>
             <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
         </div>
         <div class="bg-cambridge-400 shadow-lg">
-          <div data-iframe-width="380" data-iframe-height="240" data-share-badge-id="22498d0b-62c1-4287-b7bc-8deb03830ec0" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+          <div data-iframe-width="300" data-iframe-height="250" data-share-badge-id="22498d0b-62c1-4287-b7bc-8deb03830ec0" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
         </div>
     </div>
 </section>
