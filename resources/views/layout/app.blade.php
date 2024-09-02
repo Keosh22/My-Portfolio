41d172a040f3ce850b3bda41bfa890700dce688c
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-03 00:42:02 +0700

    feat: About me page implementation

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,7 +20,7 @@
       @include("section.badge")
     </section>
     <section class="padding">
-     
+     @includeIf("section.skills")
     </section>
     <section class="padding">
       Sign up
---
+++ resources/views/section/skills.blade.php
@@ -0,0 +1,24 @@
+<section class="max-container w-full flex lg:flex-row flex-col gap-12 justify-around items-center">
+  <div class="relative flex justify-center items-center">
+    <img class="h-28 absolute bottom-0 left-0 opacity-60 -z-10" src="{{asset("images/dots.png")}}" alt="dots">
+    <img class="h-28 absolute top-0 right-0 opacity-60 -z-10" src="{{asset("images/dots.png")}}" alt="dots">
+    <div class="h-full rounded-full overflow-hidden shadow-xl">
+      <img class="" src="{{asset("images/skills-img.png")}}" alt="">
+    </div>
+  </div>
+  <div class="flex flex-col justify-start items-start">
+    <h1 class="font-poppins text-4xl font-bold leading-normal mb-2">About <span class="text-cambridge-950">Me</span></h1>
+    <h3 class="font-poppins font-bold text-2xl leading-normal mb-1">Hello, I'm Ken</h3>
+    <p class="font-lora text-khaki-950 text-lg leading-normal lg:max-w-2xl">I'm a BSIT student at University of Rizal System Cainta - Campus, with a passion for technology and innovation. My journey into web development began with a curiosity about how things work behind the scenes in the digital world, and over the years, that curiosity has grown into a deep commitment to mastering my craft.</p>
+    <h3 class="font-poppins text-2xl font-bold mt-5 leading-normal">My Skills</h3>
+    <div class="flex flex-wrap justify-start gap-5 mt-3">
+      <img class="h-12 w-12" src="{{asset("images/Html-5.png")}}" alt="html-icon">
+      <img class="h-12 w-12" src="{{asset("images/CSs.png")}}" alt="css-icon">
+      <img class="h-12 w-12" src="{{asset("images/JavaScript.png")}}" alt="javascript-icon">
+      <img class="h-12 w-12" src="{{asset("images/laravel_icon.png")}}" alt="laravel-icon">
+      <img class="h-12 w-12" src="{{asset("images/mysql_logo.png")}}" alt="mysql-icon">
+      <img class="h-12 w-12" src="{{asset("images/Tailwind.png")}}" alt="tailwind-icon">
+      <img class="h-12 w-12" src="{{asset("images/git_icon.png")}}" alt="git-icon">
+    </div>
+  </div>
+</section>
