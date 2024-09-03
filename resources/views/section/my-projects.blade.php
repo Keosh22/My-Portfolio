9bd0e3a6a884e9f4a34b7e65595d89c24b00acbf
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-03 22:33:21 +0700

    feat: projects page content implementation

--- resources/views/section/my-projects.blade.php
+++ resources/views/section/my-projects.blade.php
@@ -1,3 +1,14 @@
 <section class="max-container w-full flex justify-center flex-col items-center">
-  <h1>My Projects</h1>
+  <h1 class="font-poppins text-4xl font-bold leading-normal mb-2">My <span class="text-cambridge-950">Projects</span></h1>
+  <h3 class="font-poppins text-lg font-semibold text-khaki-950 mb-11">Every project is a unique endeavor. See what I have accomplished so far!</h3>
+  <div class="flex flex-col gap-10 justify-center items-center">
+    <div class="flex flex-col flex-1 gap-3">
+      <img class="object-contain border-4 border-cambridge-950 rounded-xl shadow-xl" src="{{asset("images/sample_project.jpg")}}" alt="">
+      <p class="font-lora text-khaki-950 leading-normal text-lg">Homeowners Assocition Web Based System
+      </p>
+      <div class="mt-3">
+        <a href="/" class="bg-black text-champagne-200 px-6 py-3 rounded-full font-poppins leading-normal text-md shadow-md hover:bg-columbia-950 transition ease-in-out delay-150">View Project</a>
+      </div>
+    </div>
+  </div>
 </section>
