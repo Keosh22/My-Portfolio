4a6371ca7de363a0c792c117b01cd4edb2235583
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-05 17:22:20 +0700

    update: change the button style to button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<button
+    class="flex justify-center items-center bg-black text-champagne-200 font-poppins text-md leading-normal py-3 px-6 rounded-full shadow-md hover:bg-columbia-950 transition ease-out delay-150">
+    {{$slot}}
+</button>
--- resources/views/section/hero.blade.php
+++ resources/views/section/hero.blade.php
@@ -9,9 +9,12 @@
             elit. Nostrum, in mollitia voluptatum reiciendis doloribus expedita fugiat explicabo nisi voluptate dolorem.
         </p>
         <div class="flex justify-center items-start mt-10">
-            <button
-                class="flex justify-center items-center bg-black text-champagne-200 font-poppins text-md leading-normal py-3 px-6 rounded-full shadow-md font-semibold hover:bg-columbia-950 transition ease-out delay-150">Download
-                Cv</button>
+            {{-- <button
+                class="flex justify-center items-center bg-black text-champagne-200 font-poppins text-md leading-normal py-3 px-6 rounded-full shadow-md hover:bg-columbia-950 transition ease-out delay-150">Download
+                Cv</button> --}}
+                <x-button>
+                    Download Cv
+                </x-button>
         </div>
     </div>
 
