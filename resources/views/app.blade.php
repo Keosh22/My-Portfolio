5515e1f1155b4f0c27ba6be10518fd955007e068
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-09-18 23:36:37 +0700

    refactor(nav link): update navlinks to components and add attributes for reusability

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 @section("title", "Keosh's Portfolio")
 <x-layout>
     {{-- HERO SECTION --}}
-    <section class="sm:pl-16 pl-8 sm:pr-16 pr-8 sm:pb-24 pb-12">
+    <section id="hero" class="sm:pl-16 pl-8 sm:pr-16 pr-8 sm:pb-24 pb-12">
         <div class="flex flex-col xl:flex-row w-full min-h-screen max-container gap-10 justify-center">
             <x-sections.hero />
         </div>
@@ -13,19 +13,19 @@
         </div>
     </section>
     {{-- BADGE SECTION --}}
-    <section class="padding  bg-khaki-400">
+    <section id="badges" class="padding  bg-khaki-400">
         <div class="flex flex-col w-full max-container">
             <x-sections.badge />
         </div>
     </section>
     {{-- SKILLS SECTION --}}
-    <section class="padding">
+    <section id="about_me" class="padding">
         <section class="max-container w-full flex lg:flex-row flex-col gap-12 justify-around items-center my-10">
             <x-sections.skills :icons="$icons" />
         </section>
     </section>
     {{-- PROJECTS SECTION --}}
-    <section class="padding  bg-khaki-400">
+    <section id="projects" class="padding  bg-khaki-400">
         <x-sections.my-projects />
     </section>
     {{-- FOOTER SECTION --}}
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,18 +1,23 @@
 <header class="sm:px-16 px-8 py-8 w-full z-10">
     <nav class="flex justify-between items-center max-container">
-      {{-- logo --}}
-      <a href="" class="font-poppins text-2xl font-bold">Keosh</a>
-      {{-- links --}}
-      <ul class="flex justify-between items-centerflex-1 gap-10 max-lg:hidden">
-        <li class="font-poppins text-cambridge-950 text-lg leading-normal hover:text-columbia-950 transition ease-in-out delay-150"><a href="/">About Me</a></li>
-        <li class="font-poppins text-cambridge-950 text-lg leading-normal hover:text-columbia-950 transition ease-in-out delay-150"><a href="#certificates">Certificates</a></li>
-        <li class="font-poppins text-cambridge-950 text-lg leading-normal hover:text-columbia-950 transition ease-in-out delay-150"><a href="/">Achievements</a></li>
-        <li class="font-poppins text-cambridge-950 text-lg leading-normal hover:text-columbia-950 transition ease-in-out delay-150"><a href="/">Skills</a></li>
-      </ul>
-      {{-- menu --}}
-      <div class="flex justify-between items-center gap-2">
-        <button class="bg-black hover:bg-columbia-950 transition ease-in-out delay-150 text-champagne-200 px-6 py-3 rounded-full font-poppins text-md leading-normal flex justify-center items-center shadow-md">See my works</button>
-        <button class="bg-black hover:bg-columbia-950 transition ease-in-out delay-150 text-champagne-200 px-6 py-3 rounded-full font-poppins text-md leading-normal flex justify-center items-center shadow-md">Email me</button>
-      </div>
+        {{-- logo --}}
+        <a href="" class="font-poppins text-2xl font-bold">Keosh</a>
+        {{-- links --}}
+        <ul class="flex justify-between items-centerflex-1 gap-10 max-lg:hidden">
+            <li><x-link-section name="About Me" link="#about_me" class="text-cambridge-950 text-lg" /></li>
+            <li><x-link-section name="Certificates" link="#certificates" class="text-cambridge-950 text-lg" /></li>
+            <li><x-link-section name="Badges" link="#badges" class="text-cambridge-950 text-lg" /></li>
+            <li><x-link-section name="Projects" link="#projects" class="text-cambridge-950 text-lg" /></li>
+        {{-- <li class="font-poppins text-cambridge-950 text-lg leading-normal hover:text-columbia-950 transition ease-in-out delay-150"><a href="/">Skills</a></li> --}}
+        </ul>
+        {{-- menu --}}
+        <div class="flex justify-between items-center gap-2">
+            <button
+                class="bg-black hover:bg-columbia-950 transition ease-in-out delay-150 text-champagne-200 px-6 py-3 rounded-full font-poppins text-md leading-normal flex justify-center items-center shadow-md">See
+                my works</button>
+            <button
+                class="bg-black hover:bg-columbia-950 transition ease-in-out delay-150 text-champagne-200 px-6 py-3 rounded-full font-poppins text-md leading-normal flex justify-center items-center shadow-md">Email
+                me</button>
+        </div>
     </nav>
-  </header>
+</header>
